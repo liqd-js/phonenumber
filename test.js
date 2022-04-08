cb8fe83c91988b6cedfee827eb643013e8079d9b
Tomáš Korenko <radixxko@gmail.com>
2022-04-08 12:01:38 +0200

    Version 1.0.1

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
 const PhoneNumber = require('./lib/phonenumber');
 
+/**/
 const tests = 
 [
     [ '951002064' ],
     [ '951002064', 'SK' ],
-    [ '000951002064', 'SK' ]
+    [ '000951002064', 'SK' ],
+    [ '421901426214', 'CZ' ],
+    [ '421901426214', 'CZ', [ 'SK', 'CZ' ]]
 ]
+/**/
 
 for( let [ number, country ] of tests )
 {
     console.log( 'Parse', { number, country, parsed: PhoneNumber.parse( number, country )});
 }
 
-for( let [ number, country ] of tests )
+for( let [ number, country, countries ] of tests )
 {
-    console.log( 'Repair', { number, country, repaired: PhoneNumber.repair( number, country )});
+    console.log( 'Repair', { number, country, countries, repaired: PhoneNumber.repair( number, countries || country )});
 }
